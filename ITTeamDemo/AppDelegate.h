25fd2f729f6751d4fd12da6356ec53afe0bbd069
FroidWinter <mac@dong.local>
2016-06-24 17:04:54 +0800

    forid
    ddd

---
+++ ITTeamDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ITTeamDemo
+//
+//  Created by mac on 16/6/24.
+//  Copyright © 2016年 dong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
